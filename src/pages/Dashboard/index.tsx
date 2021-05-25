968d1a138e704aa6f5c6f8d3b9612f04d9c876bf
Charles Pereira <charlesti.ptu@gmail.com>
2021-05-25 12:04:20 -0300

    worker with RN respansive fontSize, RFPercentage and RFValue

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/54192694?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Charles</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
