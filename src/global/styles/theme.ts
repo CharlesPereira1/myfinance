0e5def900a9e6092df4a3526e46b3b1273d909ed
Charles Pereira <charlesti.ptu@gmail.com>
2021-05-25 10:51:31 -0300

    styled components with themeProvider and typeof theme extends

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: "#5636D3",
+    secundary: "#ff872c",
+    secundary_light: "rgba(255, 135, 44, 0.3)",
+
+    seccess: "#12a454",
+    seccess_light: "rgba(18, 164, 84, 0.5)",
+
+    attention: "#e83f5b",
+    attention_light: "rgba(232, 63, 91, 0.5)",
+
+    shape: "#fff",
+    title: "#363f6f",
+    text: "#969cb2",
+    background: "#f0f2f5",
+  },
+};
